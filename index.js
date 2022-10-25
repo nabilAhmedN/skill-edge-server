b413924e20c58930a4f35bdc4b9e7cbbee72fa45
nabilAhmedN <nabilahmednahid@gmail.com>
2022-10-26 01:17:10 +0600

    server site is ready to work

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+
+
+
+app.get("/", (req, res) => {
+  res.send("Course API Running");
+});
+
+
+
+app.listen(port, () => {
+  console.log(" running on port", port);
+});
