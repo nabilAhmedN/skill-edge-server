25a78d2e4f0c736d84eeafb698c5502c9086bd30
nabilAhmedN <nabilahmednahid@gmail.com>
2022-10-26 02:00:35 +0600

    course data updated

--- index.js
+++ index.js
@@ -5,13 +5,23 @@ const port = process.env.PORT || 5000;
 
 app.use(cors());
 
-
+const courses = require("./data/course.json");
+const coursesFeat = require("./data/coursesFeatures.json");
 
 app.get("/", (req, res) => {
   res.send("Course API Running");
 });
 
+app.get("/courses", (req, res) => {
+  res.send(courses);
+});
 
+app.get("/courses/:id", (req, res) => {
+    const id = req.params.id;
+    const courseData = coursesFeat.find((n) => n.course_id === id);
+
+    res.send(courseData);
+});
 
 app.listen(port, () => {
   console.log(" running on port", port);
